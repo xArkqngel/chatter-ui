fc765431d5559af8347a2ad66cd46a141e87d48c
xArkqngel <arkqngel343@gmail.com>
2024-07-19 10:15:39 -0500

    CORS fixed adding proxy to frontend, signup working OK

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 function SignUp() {
+  const [createUser] = useCreateUser();
+
   return (
-    <Auth submitLabel="Sign Up" onSubmit={async () => {}}>
-      <Link to="/login" style={{alignSelf:'center'}}>
+    <Auth
+      submitLabel="Sign Up"
+      onSubmit={async ({email, password}) => {
+        await createUser({
+          variables: {
+            createUserInput: {
+              email,
+              password,
+            },
+          },
+        });
+      }}
+    >
+      <Link to="/login" style={{ alignSelf: "center" }}>
         <MUILink>Log In</MUILink>
       </Link>
     </Auth>
