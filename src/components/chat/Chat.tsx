c3bb9e046a0226fc400adb55ca8ff6584ab0c75e
xArkqngel <arkqngel343@gmail.com>
2024-08-05 21:00:51 -0500

    resposive fixes and message styling

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,16 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useGetChat } from "../../hooks/useGetChat";
 import {
+  Avatar,
   Box,
   Divider,
+  Grid,
   IconButton,
   InputBase,
   Paper,
   Stack,
+  Typography,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useGetMessages } from "../../hooks/useGetMessages";
 
 function Chat() {
@@ -20,16 +23,50 @@ function Chat() {
   const { data } = useGetChat({ _id: chatId });
   const [createMessage] = useCreateMessage(chatId);
   const { data: messages } = useGetMessages({ chatId });
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const location = useLocation();
+
+  const scrollToBottom = () => {
+    divRef.current?.scrollIntoView();
+  };
+
+  const handleCreateMessage = async () => {
+    await createMessage({
+      variables: { createMessageInput: { content: message, chatId } },
+    });
+    setMessage("");
+    scrollToBottom();
+  };
+
+  useEffect(() => {
+    setMessage("");
+    scrollToBottom();
+  }, [location, messages]);
 
   return (
     <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
       <h1>{data?.chat.name}</h1>
-      <Box>
+      <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>
         {messages?.messages.map((message) => (
-          <div key={message._id}>
-            <p>{message.content}</p>
-          </div>
+          <Grid container alignItems="center" marginBottom="1rem">
+            <Grid item xs={2} lg={1}>
+              <Avatar alt="Remy Sharp" src="" sx={{ width: 52, height: 52 }} />
+            </Grid>
+            <Grid item xs={10} lg={11}>
+              <Stack>
+                <Paper sx={{ width: "fit-content" }}>
+                  <Typography sx={{ padding: "0.9rem" }}>
+                    {message.content}
+                  </Typography>
+                </Paper>
+                <Typography variant="caption" sx={{ marginLeft: "0.25rem" }}>
+                  {new Date(message.createdAt).toLocaleTimeString()}
+                </Typography>
+              </Stack>
+            </Grid>
+          </Grid>
         ))}
+        <div ref={divRef}></div>
       </Box>
       <Paper
         sx={{
@@ -38,6 +75,7 @@ function Chat() {
           justifySelf: "flex-end",
           alignItems: "center",
           width: "100%",
+          margin: '1rem 0'
         }}
       >
         <InputBase
@@ -45,14 +83,15 @@ function Chat() {
           placeholder="Type a message"
           onChange={(e) => setMessage(e.target.value)}
           value={message}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleCreateMessage();
+            }
+          }}
         />
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
-          onClick={() =>
-            createMessage({
-              variables: { createMessageInput: { content: message, chatId } },
-            })
-          }
+          onClick={() => handleCreateMessage()}
           color="primary"
           sx={{ p: "10px" }}
         >
