19a7b6d0250236d45805c65cb7b7fa3b9176c904
xArkqngel <arkqngel343@gmail.com>
2024-08-08 10:59:51 -0500

    Implemented link splitter, for http and web sockets requests. Refetch and sort messages by date

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -13,22 +13,28 @@ import {
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useGetMessages } from "../../hooks/useGetMessages";
+import { useMessageCreated } from "../../hooks/useMessageCreated";
 
-function Chat() {
+const Chat = () => {
   const params = useParams();
   const [message, setMessage] = useState("");
   const chatId = params._id!;
   const { data } = useGetChat({ _id: chatId });
-  const [createMessage] = useCreateMessage(chatId);
+  const [createMessage] = useCreateMessage();
   const { data: messages } = useGetMessages({ chatId });
   const divRef = useRef<HTMLDivElement | null>(null);
   const location = useLocation();
 
-  const scrollToBottom = () => {
-    divRef.current?.scrollIntoView();
-  };
+  useMessageCreated({ chatId });
+
+  const scrollToBottom = () => divRef.current?.scrollIntoView();
+
+  useEffect(() => {
+    setMessage("");
+    scrollToBottom();
+  }, [location.pathname, messages]);
 
   const handleCreateMessage = async () => {
     await createMessage({
@@ -38,34 +44,39 @@ function Chat() {
     scrollToBottom();
   };
 
-  useEffect(() => {
-    setMessage("");
-    scrollToBottom();
-  }, [location, messages]);
-
   return (
     <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
       <h1>{data?.chat.name}</h1>
       <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>
-        {messages?.messages.map((message) => (
-          <Grid container alignItems="center" marginBottom="1rem">
-            <Grid item xs={2} lg={1}>
-              <Avatar alt="Remy Sharp" src="" sx={{ width: 52, height: 52 }} />
-            </Grid>
-            <Grid item xs={10} lg={11}>
-              <Stack>
-                <Paper sx={{ width: "fit-content" }}>
-                  <Typography sx={{ padding: "0.9rem" }}>
-                    {message.content}
-                  </Typography>
-                </Paper>
-                <Typography variant="caption" sx={{ marginLeft: "0.25rem" }}>
-                  {new Date(message.createdAt).toLocaleTimeString()}
-                </Typography>
-              </Stack>
-            </Grid>
-          </Grid>
-        ))}
+        {messages &&
+          [...messages.messages]
+            .sort(
+              (messageA, messageB) =>
+                new Date(messageA.createdAt).getTime() -
+                new Date(messageB.createdAt).getTime()
+            )
+            .map((message) => (
+              <Grid container alignItems="center" marginBottom="1rem">
+                <Grid item xs={2} lg={1}>
+                  <Avatar src="" sx={{ width: 52, height: 52 }} />
+                </Grid>
+                <Grid item xs={10} lg={11}>
+                  <Stack>
+                    <Paper sx={{ width: "fit-content" }}>
+                      <Typography sx={{ padding: "0.9rem" }}>
+                        {message.content}
+                      </Typography>
+                    </Paper>
+                    <Typography
+                      variant="caption"
+                      sx={{ marginLeft: "0.25rem" }}
+                    >
+                      {new Date(message.createdAt).toLocaleTimeString()}
+                    </Typography>
+                  </Stack>
+                </Grid>
+              </Grid>
+            ))}
         <div ref={divRef}></div>
       </Box>
       <Paper
@@ -75,23 +86,23 @@ function Chat() {
           justifySelf: "flex-end",
           alignItems: "center",
           width: "100%",
-          margin: '1rem 0'
+          margin: "1rem 0",
         }}
       >
         <InputBase
           sx={{ ml: 1, flex: 1, width: "100%" }}
-          placeholder="Type a message"
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={(event) => setMessage(event.target.value)}
           value={message}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleCreateMessage();
+          placeholder="Message"
+          onKeyDown={async (event) => {
+            if (event.key === "Enter") {
+              await handleCreateMessage();
             }
           }}
         />
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
-          onClick={() => handleCreateMessage()}
+          onClick={handleCreateMessage}
           color="primary"
           sx={{ p: "10px" }}
         >
@@ -100,6 +111,6 @@ function Chat() {
       </Paper>
     </Stack>
   );
-}
+};
 
 export default Chat;
--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,8 +1,11 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { API_URL } from "./urls";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { API_URL, WS_URL } from "./urls";
 import { onError } from "@apollo/client/link/error";
 import excludedRoutes from "./excluded-routes";
 import { onLogout } from "../utils/logout";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 /**
  * This link is used to intercept the errors that come from the server.
@@ -11,7 +14,7 @@ import { onLogout } from "../utils/logout";
 const logoutLink = onError((error) => {
   if (
     error.graphQLErrors?.length &&
-    (error.graphQLErrors[0].extensions.originalError as any).statusCode === 401
+    (error.graphQLErrors[0].extensions?.originalError as any)?.statusCode === 401
   ) {
     if (!excludedRoutes.includes(window.location.pathname)) {
       onLogout();
@@ -23,9 +26,31 @@ const httpLink = new HttpLink({
   uri: `${API_URL}/graphql`, // This is the URL of the GraphQL server
 });
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `${WS_URL}/graphql`, // This is the URL of the WebSocket server
+  })
+);
+
+/**
+ * This link is used to split the requests between the HTTP and WebSocket links.
+ * If the request is a subscription, it will be sent to the WebSocket server, otherwise, it will be sent to the HTTP server.
+ */
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition?.kind === "OperationDefinition" &&
+      definition?.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
   cache: new InMemoryCache(), // This is the cache that Apollo uses to store the data, it is used to avoid making the same request multiple times
-  link: logoutLink.concat(httpLink), // This makes the client to connect first to the backend, and then to the logoutLink
+  link: logoutLink.concat(splitLink), // This makes the client to connect first to the backend, and then to the logoutLink
 });
 
 export default client;
