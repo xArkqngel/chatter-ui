69169668c8118d18ea8a15d6a5b830933c2a6d58
xArkqngel <arkqngel343@gmail.com>
2024-08-26 18:20:29 -0500

    pass jwt

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,8 @@
-export const setToken = (token: string) => {};
+export const setToken = (token: string) => localStorage.setItem('token', token);
 
 export const getToken = () => {
-  return "";
-};
+    const token = localStorage.getItem('token');
+    return token ? `Bearer ${token}` : '';
+}
 
 export const clearToken = () => localStorage.clear();
